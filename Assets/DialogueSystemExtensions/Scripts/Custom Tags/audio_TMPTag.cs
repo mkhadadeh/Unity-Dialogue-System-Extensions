5dbf35bc7deca39afb571ff927706a293660be79
Mohammed Khadadeh <mkhadadeh@scu.edu>
2020-01-01 02:17:59 -0800

    Created a new tag for audio playback
    Not implemented yet.

---
+++ Assets/DialogueSystemExtensions/Scripts/Custom Tags/audio_TMPTag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioTag", menuName = "CustomTags/AudioTag")]
+public class audio_TMPTag : CustomTMPTag
+{
+
+    public override string tag_name
+    {
+        get
+        {
+            return "audio";
+        }
+    }
+
+    public override bool needs_closing_tag
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override IEnumerator applyToText(TMPro.TextMeshPro text, int startIndex, int length, string param)
+    {
+        // Find the audio player and play the sound param from it.
+        yield return new WaitForSeconds(0.1f);
+    }
+}
